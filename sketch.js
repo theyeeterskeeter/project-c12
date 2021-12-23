fc42be815d484054e79970f7f8d5fd885520edc4
theyeeterskeeter <93965788+theyeeterskeeter@users.noreply.github.com>
2021-12-23 17:09:26 +1100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -35,34 +35,9 @@ function draw() {
   rabbit.collide(edges);
   
    drawSprites();
-   
-  
-// var select_sprites = Math(random(1,3));
-
-// var select_sprites = Math.random(random(1,3));
-
-// var select_sprites = Math.round(1,3);
 
 var select_sprites = Math.round(random(1,3));
 
-  
-  // if (frameCount % 50 == 0) {
-  //   if (select_sprites == 1) {
-  //     createApples();
-  //   } else if (select_sprites == 2) {
-  //     createOrange();
-  //   }else {
-  //     createRed();
-  //   }
-  // }
-
-  // if (frameCount % 80 == 0) {
-  //   if (select_sprites == 1) {
-  //     createApples();
-  //   } else if (select_sprites == 2) {
-  //     createOrange();
-  //   }
-  // }
 
   if (frameCount / 80 == 0) {
     if (select_sprites == 1) {
@@ -72,17 +47,7 @@ var select_sprites = Math.round(random(1,3));
     }else {
       createRed();
     }
-  // }
-
-  // if (frameCount % 80 = 0) {
-  //   if (select_sprites == 1) {
-  //     createApples();
-  //   } else if (select_sprites == 2) {
-  //     createOrange();
-  //   }else {
-  //     createRed();
-  //   }
-  // }
+  }
 
 
 
